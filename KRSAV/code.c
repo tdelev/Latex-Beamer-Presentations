d955cb6f66dcc416d09b6737f34021cd8f814ff9
tdelev <tomce.delev@gmail.com>
2012-09-27 23:37:55 +0200

    Latest changes (av1 and av2 merged)

--- KRSAV/code.c
+++ KRSAV/code.c
@@ -1,35 +1,19 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#define MAX 100
-
-void trim(char *str) {
-	int i;
-	while (isspace(str[0])) {
-		for (i = 0; str[i] != '\0'; i++)
-			str[i] = str[i + 1];
-	}
-	for (i = strlen(str) - 1; isspace(str[i]); i--)
-		;
-	str[i + 1] = '\0';
-}
-void trim1(char *s) {
-	char *d = s;
-	while (isspace(*s++))
-		;
-	s--;
-	while (*d++ = *s++)
-		;
-	d--;
-	while (isspace(*--d))
-		*d = 0;
-}
+#include <iostream>
+using namespace std;
 int main() {
-	char s[MAX];
-	printf("Vnesete string: ");
-	gets(s);
-	printf("[%s] -> ", s);
-	trim1(s);
-	printf("[%s]", s);
-	return 0;
+	cout << "Vnesi gi tvoite godini: ";
+	int myAge;
+	cin >> myAge;
+	cout << "Vnesi gi godinite na prijatel: ";
+	int friendsAge;
+	cin >> friendsAge;
+if (myAge > friendsAge)
+cout << "Ti se postar.\n";
+else
+if (myAge < friendsAge)
+cout << "Prijatelot e postar.\n";
+else
+cout << "Ti i prijatelot imate
+godini.\n";
+return 0;
 }
